c36ca949971266fcb55d8b9b1cfc863c8d106dc1
ttichila@uci.edu <ttichila@uci.edu>
2025-02-01 15:00:32 -0800

    loop to check if url in domain

--- scraper.py
+++ scraper.py
@@ -25,6 +25,12 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+        #return only urls that are within the domain and paths
+        validDomain = [".ics.uci.edu/", ".cs.uci.edu/", ".informatics.uci.edu/", ".stat.uci.edu/"]
+        for i in validDomain:
+            if i in url:
+                return True
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
